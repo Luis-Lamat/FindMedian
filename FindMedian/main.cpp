4aadba0d2dca67be62b68d2ba7084ddb94b0ffd4
Luis-Lamat <beto_alaska@hotmail.com>
2014-10-05 21:11:09 -0500

    Adds user inputs

--- FindMedian/main.cpp
+++ FindMedian/main.cpp
@@ -2,17 +2,42 @@
 //  main.cpp
 //  FindMedian
 //
-//  Created by Luis Alberto Lamadrid on 10/5/14.
+//  Created by Luis Alberto Lamadrid &
+//             Paulina Escalante Campbell
+//             on 10/4/14.
+//
 //  Copyright (c) 2014 Luis Alberto Lamadrid. All rights reserved.
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[])
+int main()
 {
+    // variable declaration
+    int cases, array_size, num;
+    
+    // case input
+    cin >> cases;
+    
+    // case while loop
+    while (cases--) {
+        
+        /* --- USER ARRAY DATA INPUT START --- */
+        cin >> array_size;
+        int arr1[array_size] ,arr2[array_size];
+        
+        for (int i = 0; i < array_size; i++) {
+            cin >> num; arr1[i] = num; }
+        
+        for (int i = 0; i < array_size; i++) {
+            cin >> num; arr2[i] = num; }
+        /* --- USER ARRAY DATA INPUT END ----- */
+        
+    }
+    
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
 
