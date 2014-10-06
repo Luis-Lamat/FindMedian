57d5708fcb51d1933fdbb5e6a2c805064ed60e20
Luis-Lamat <beto_alaska@hotmail.com>
2014-10-05 22:44:15 -0500

    Adds O(log n) order to the algorithm, works as expected...

--- FindMedian/main.cpp
+++ FindMedian/main.cpp
@@ -12,10 +12,35 @@
 #include <iostream>
 using namespace std;
 
+// Minimum and maximum helper functions
+int max(int a, int b) { return (a > b) ? a : b; }
+int min(int a, int b) { return (a < b) ? a : b; }
+
+double FindMedian(int *a1, int s1, int e1, int *a2, int s2, int e2){
+    int mid1 = (s1 + e1) / 2;
+    int mid2 = (s2 + e2) / 2;
+    
+    if ((e1 - s1 == 1) && (e2 - s2 == 1)){
+        double num1 = max(a1[s1], a2[s2]);
+        double num2 = min(a1[e1], a2[e2]);
+        return (num1 + num2) / 2;
+    }
+    
+    if (a1[mid1] < a2[mid2]) {
+        return FindMedian(a1, mid1, e1, a2, s2, mid2);
+    }
+    else if ( a1[mid1] > a2[mid2]){
+        return FindMedian(a1, s1, mid1, a2, mid2, e2);
+    }
+    
+    // returning an error...
+    return -1;
+}
+
 int main()
 {
     // variable declaration
-    int cases, array_size, num;
+    int cases, array_size, num, count = 1;
     
     // case input
     cin >> cases;
@@ -27,16 +52,19 @@ int main()
         cin >> array_size;
         int arr1[array_size] ,arr2[array_size];
         
-        for (int i = 0; i < array_size; i++) {
+        array_size--;
+        
+        for (int i = 0; i <= array_size; i++) {
             cin >> num; arr1[i] = num; }
         
-        for (int i = 0; i < array_size; i++) {
+        for (int i = 0; i <= array_size; i++) {
             cin >> num; arr2[i] = num; }
         /* --- USER ARRAY DATA INPUT END ----- */
         
+        cout << "Median case " << count << ": ";
+        cout << FindMedian(arr1, 0, array_size, arr2, 0, array_size) << endl;
+        count++;
     }
-    
-
     
     return 0;
 }
